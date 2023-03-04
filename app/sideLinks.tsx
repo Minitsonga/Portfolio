167e998a2f28feb339f5b08f08001755d430f177
Minitsonga <minitsonga2002@gmail.com>
2023-03-04 15:24:55 +0100

    Trying to add icons

---
+++ app/sideLinks.tsx
@@ -0,0 +1,21 @@
+import "./pageInfo.css";
+
+export default function SideLinks() {
+  return (
+    <div>
+      <ul>
+        <li>
+        </li>
+        <li>
+          <a href=""></a>
+        </li>
+        <li>
+          <a href=""></a>
+        </li>
+        <li>
+          <a href=""></a>
+        </li>
+      </ul>
+    </div>
+  );
+}
