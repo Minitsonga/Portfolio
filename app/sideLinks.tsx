acfffae79cfed8e23136424dcee7a9f61a818076
Minitsonga <minitsonga2002@gmail.com>
2023-03-05 04:03:32 +0100

    Finaly added icons but trying to find the goods ones

--- app/sideLinks.tsx
+++ app/sideLinks.tsx
@@ -1,19 +1,40 @@
 import "./pageInfo.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  solid,
+  regular,
+  thin,
+  brands,
+  icon,
+} from "@fortawesome/fontawesome-svg-core/import.macro";
+
+import { UitGithubAlt, UitLinkedinAlt, UitTwitterAlt  } from "@iconscout/react-unicons-thinline";
+
 
 export default function SideLinks() {
   return (
-    <div>
+    <div className="w-12 fixed bottom-0 left-10 right-auto">
       <ul>
         <li>
+          <a className="hover:text-bluey-snd cursor-pointer" href="">
+            <UitGithubAlt />
+          </a>
         </li>
         <li>
-          <a href=""></a>
+          <a className="hover:text-bluey-snd cursor-pointer" href="">
+            <UitLinkedinAlt />
+          </a>
         </li>
         <li>
-          <a href=""></a>
+          <a className="hover:text-bluey-snd cursor-pointer" href="">
+            <UitTwitterAlt />
+          </a>
         </li>
+
         <li>
-          <a href=""></a>
+          <a className="hover:text-bluey-snd cursor-pointer" href="">
+            <FontAwesomeIcon icon={thin("globe")} />
+          </a>
         </li>
       </ul>
     </div>
