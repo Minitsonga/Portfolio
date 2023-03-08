aad313c1edb99cbc4e1417ff884a230b6a15b219
Minitsonga <minitsonga2002@gmail.com>
2023-03-08 16:22:08 +0100

    Finished main section starting about section

--- app/page.tsx
+++ app/page.tsx
@@ -3,20 +3,32 @@ import "./pageInfo.css";
 export default function PageInfo() {
   return (
     <main className="px-12 md:px-28 xl:px-32 max-2xl:px-0">
-      <div className="content">
+      {/* <section className="content text-center mb-28">
         <h1 className="blueText text-8xl mb-4">Julien Vandamme</h1>
-        <p className="pl-16 text-5xl mb-8">
-          <span className="opacity-70">
-            Je suis etudiant en informatique et je veux devenir
-          </span>
-          <span className="blueText opacity-85"> développeur</span>
-          <span className="opacity-70"> de jeux vidéo.</span>
+        <p className="pl-20 pr-24 text-5xl opacity-70 mb-5">
+          Passioné par le développement de jeux vidéo et par le web
         </p>
-        <p className="text-xl pl-32 w-2/3">
-          Je suis passionné d'informatique depuis mes 12 ans et
-          particuliairement par le monde des jeux vidéo.
+        <p className="text-2xl px-40 mx-32">
+          Je suis étudiant en informatique. J'adore coder, créer des jeux,
+          apprendre de nouvelles choses.
         </p>
-      </div>
+         <button className="btn m-auto mt-8">
+          <a className="text-center text-xl" href="/resume.pdf" target="_blank">
+            Mon CV
+          </a>
+        </button>
+      </section> */}
+
+      <section id="about" className="content mt-14">
+        <h1 className="flex blueText pl-28 text-6xl mb-4">[ <p className="text-white text-4xl m-auto px-5">About me</p>]</h1>
+        <p className="pl-20 pr-24 text-5xl opacity-70 mb-5">
+          Passioné par le développement de jeux vidéo et par le web
+        </p>
+        <p className="text-2xl px-40 mx-32">
+          Je suis étudiant en informatique. J'adore coder, créer des jeux,
+          apprendre de nouvelles choses.
+        </p>
+      </section>
     </main>
   );
 }
