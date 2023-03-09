6cef2d1ca257a608c4553648940970a016304abc
Minitsonga <minitsonga2002@gmail.com>
2023-03-09 02:02:48 +0100

    Modified About with line and need to add img now

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
 import "./pageInfo.css";
 
+
 export default function PageInfo() {
   return (
     <main className="px-12 md:px-28 xl:px-32 max-2xl:px-0">
-      {/* <section className="content text-center mb-28">
+      <section className="content text-center mb-28">
         <h1 className="blueText text-8xl mb-4">Julien Vandamme</h1>
         <p className="pl-20 pr-24 text-5xl opacity-70 mb-5">
           Passioné par le développement de jeux vidéo et par le web
@@ -17,10 +18,13 @@ export default function PageInfo() {
             Mon CV
           </a>
         </button>
-      </section> */}
+      </section>
 
       <section id="about" className="content mt-14">
-        <h1 className="flex blueText pl-28 text-6xl mb-4">[ <p className="text-white text-4xl m-auto px-5">About me</p>]</h1>
+        <h1 className="flex title blueText pl-8 text-6xl mb-8">
+          [ <p className="text-white text-4xl m-auto px-5">About me</p>]
+        </h1>
+        
         <p className="pl-20 pr-24 text-5xl opacity-70 mb-5">
           Passioné par le développement de jeux vidéo et par le web
         </p>
