73ac57ef87815de3cae62d7f003a0dd53546678c
Minitsonga <minitsonga2002@gmail.com>
2023-03-10 01:33:16 +0100

    modified first paragraphe

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
 import "./pageInfo.css";
 
-
 export default function PageInfo() {
   return (
     <main className="px-12 md:px-28 xl:px-32 max-2xl:px-0">
@@ -13,7 +12,7 @@ export default function PageInfo() {
           Je suis étudiant en informatique. J'adore coder, créer des jeux,
           apprendre de nouvelles choses.
         </p>
-         <button className="btn m-auto mt-8">
+        <button className="btn m-auto mt-8">
           <a className="text-center text-xl" href="/resume.pdf" target="_blank">
             Mon CV
           </a>
@@ -24,14 +23,19 @@ export default function PageInfo() {
         <h1 className="flex title blueText pl-8 text-6xl mb-8">
           [ <p className="text-white text-4xl m-auto px-5">About me</p>]
         </h1>
-        
-        <p className="ml-26 pl-10 pr-40 text-md mb-5">
-          Salut ! Je m'appelle Julie Vandamme et je suis actuellement étudiant en informatique.
-          Je suis passionné par les jeux vidéo depuis mes 12 ans et il y a 3 ans je me suis interréssé au monde du web.
-          J'ai créé plusieurs jeux et sites internet qui m'ont permi d'apprendre le <span>C# sur Unity</span> et <span>NodeJs</span>
-          , <span>Javascript</span>, <span>ReactJs</span>.
-        </p>
-       
+
+        <div className="flex">
+          <p className="ml-26 pl-10 w-2/3 pr-40 text-md mb-5">
+            Salut ! Je m'appelle Julie Vandamme et je suis actuellement étudiant
+            en informatique. Je suis passionné par les jeux vidéo depuis mes 12
+            ans et il y a 3 ans, je me suis intéressé au monde du web. J'ai créé
+            plusieurs jeux et sites internet qui m'ont permis d'apprendre le{" "}
+            <span>C# sur Unity</span> et <span>Node.js</span>,{" "}
+            <span>Javascript</span>, <span>ReactJs</span>.
+          </p>
+
+          <p>IMAGE ICI</p>
+        </div>
       </section>
     </main>
   );
